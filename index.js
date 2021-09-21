f5e4d825eda9c194b6a359bb693eaba68047fc38
Lucas Feijó <lucasfjportela@gmail.com>
2021-09-20 21:04:15 -0300

    Add music queue

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const bot = new Client({
 })
 const token = `${process.env.APPLICATION_TOKEN}`
 bot.login(token)
+var queue = []
 
 function parseMessage(content) {
   const message = content.toLowerCase().split(' ')
@@ -27,7 +28,7 @@ function parseMessage(content) {
 function playerMonitor(player) {
   player.on(AudioPlayerStatus.Idle, () => console.log('Player status: IDLE'))
   player.on(AudioPlayerStatus.Buffering, () => console.log('Player status: BUFFERING'))
-  player.on(AudioPlayerStatus.Playing, () => console.log('Player status: PLAYING'))
+  player.on(AudioPlayerStatus.Playing, () => console.log('Player status: PLAYING\nIn queue: ', queue.length - 1))
   player.on(AudioPlayerStatus.Paused, () => console.log('Player status: PAUSED'))
   player.on(AudioPlayerStatus.AutoPaused, () => console.log('Player status: AUTOPAUSED'))
 }
@@ -62,24 +63,42 @@ bot.on('messageCreate', async message => {
     }
 
     try {
+      const IDLE_STATE = AudioPlayerStatus.Idle || AudioPlayerStatus.AutoPaused
       const stream = await ytdl(`https://www.youtube.com/watch?v=${song.id}`, {
         highWaterMark: 1 << 25,
         filter: 'audioonly',
         format: 'mp3',
         quality: 'highestaudio'
       })
-
       const player = createAudioPlayer()
       const resource = createAudioResource(stream, { inputType: StreamType.Opus })
-      connection.subscribe(player)
 
-      // Play
-      player.play(resource)
-      message.reply(`Lansei a braba: ${song.title} (${song.durationFormatted})`)
+      if (queue.length === 0 && IDLE_STATE) {
+        connection.subscribe(player)
+        queue.push(resource)
+        player.play(resource)
+        message.reply(`Lansei a braba: ${song.title} (${song.durationFormatted})`)
+      } else if (AudioPlayerStatus.Playing) {
+        queue.push(resource)
+        message.reply(`Essa braba foi pra fila: ${song.title} (${song.durationFormatted})`)
+      }
 
-      // Player monitor
+      player.on(AudioPlayerStatus.Idle, () => {
+        console.log('Player status: IDLE')
+        if (queue.length === 0) {
+          return
+        }
+
+        queue.shift()
+        if (queue.length > 0) {
+          player.play(queue[0])
+          message.reply(`Lansei a braba: ${song.title} (${song.durationFormatted})`)
+        }
+      })
+
       playerMonitor(player)
     } catch (error) {
+      queue = []
       connection.disconnect()
       console.log('Erro: ', error)
     }
